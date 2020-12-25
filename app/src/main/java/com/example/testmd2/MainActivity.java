be923e6b1def3926ad69f4f7c442dbf1c039a59a
Sarhan <https://github.com/MohamedKhaled2268/TestMD2>
2020-12-25 21:26:24 +0200

    Hello

--- app/src/main/java/com/example/testmd2/MainActivity.java
+++ app/src/main/java/com/example/testmd2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("I Love Dalia");
-        System.out.println("iiofg");
+        System.out.println("");
     }
 }
