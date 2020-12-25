66e8075b15d2eed9832b8085429c601b2493c07e
Mohamed And Dalia <https://github.com/MohamedKhaled2268/TestMD2>
2020-12-25 21:25:57 +0200

    HI

--- app/src/main/java/com/example/testmd2/MainActivity.java
+++ app/src/main/java/com/example/testmd2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("I Love Dalia");
+        System.out.println("iiofg");
     }
 }
